43a1747a01ac1d3fa1d4843f766f98d7831f779f
Julien Flamant <flamantjf@gmail.com>
2018-03-26 23:54:27 +0200

    update setup.py with dependencies

--- setup.py
+++ setup.py
@@ -19,21 +19,21 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sample',
+    name='2018-zeros_spectrogram_white_noise',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.1.dev',
 
-    description='A sample Python project',
+    description='A companion set of notebooks for the paper Zeros of the spectrogram of white noise',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/[myself]/[project_name]',
+    url='https://github.com/jflamant/2018-zeros_spectrogram_white_noise',
 
     # Author details
-    author='[myself]',
+    author='Remi Bardenet, Julien Flamant, Pierre Chainais',
     author_email='[myself]@[mydomain].com',
 
     # Choose your license
@@ -49,23 +49,17 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='time-frequency analysis, Gaussian Analytic Functions, zeros',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -80,9 +74,12 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'nose                 >=1.3.0,<1.8.9',
-        'coverage             >=3.5.3',
         'matplotlib',
+        'numpy',
+        'scipy',
+        'seaborn',
+        'cmocean',
+        'rpy2'
     ],
 
     # List additional groups of dependencies here (e.g. development
@@ -97,15 +94,15 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['package_data.dat'],
-    },
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
